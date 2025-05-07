248da552a3298eb6901c7c601961248704c32a32
seledun <s.ledung@gmail.com>
2025-05-07 21:28:11 +0200

    TDD: Test for easter egg functionality (not passing)

--- tests/browser.test.js
+++ tests/browser.test.js
@@ -23,6 +23,19 @@ test('The stack should be empty in the beginning', async () => {
     expect(stack).toEqual("n/a");
 });
 
+test('peeking an empty stack exactly 42 times in a row adds a good movie to the stack', async () => {
+    let stack = await driver.findElement(By.id('peek'));
+    for (let i = 0; i < 41; i++) {
+        stack.click();
+        let text = await driver.findElement(By.id('top_of_stack')).getText();
+        expect(text).toEqual("undefined");    
+    }
+    
+    stack.click();
+    let text = await driver.findElement(By.id('top_of_stack')).getText();
+    expect(text).toEqual("Hitchhiker's guide to the galaxy")
+});
+
 describe('Clicking "Pusha till stacken"', () => {
     it('should open a prompt box', async () => {
         let push = await driver.findElement(By.id('push'));
